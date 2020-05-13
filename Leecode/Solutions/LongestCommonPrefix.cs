0fb48bdb3787ac8b4d8e71a2f9055949157dfd80
lyt <numb327@gmail.com>
2020-05-13 15:32:33 +0800

    优化算法

--- Leecode/Solutions/LongestCommonPrefix.cs
+++ Leecode/Solutions/LongestCommonPrefix.cs
@@ -1,22 +1,28 @@
-﻿using System.Collections.Generic;
-using System;
-using System.Text;
-
-namespace Leecode.Solutions
+﻿namespace Leecode.Solutions
 {
     public static class LongestCommonPrefix
     {
-        public static string LongestCommonPrefixFun1(string[] strs)
+        public static string LongestCommonPrefixFun(string[] strs)
         {
             if (strs == null || strs.Length == 0) return "";
 
             string prefix = strs[0];
+            // 找空字符串
+            foreach (var str in strs)
+            {
+                if (string.IsNullOrWhiteSpace(str))
+                {
+                    prefix = str;
+                    break;
+                }
+            }
 
             for (int i = 1; i < strs.Length; i++)
             {
                 while (strs[i].IndexOf(prefix) != 0)
                 {
-                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    // prefix = prefix.Substring(0, prefix.Length - 1);
+                    prefix = prefix[0..^1];
                     if (string.IsNullOrWhiteSpace(prefix)) return "";
                 }
             }
