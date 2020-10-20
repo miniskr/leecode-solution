11d6e241ce1e1d05372403592c3f9afd0601697d
liuyt <numb327@gmail.com>
2020-10-20 10:21:23 +0800

    验证括号

---
+++ Leecode.Test/ValidParentheses.cs
@@ -0,0 +1,27 @@
+﻿using Leecode.Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class ValidParenthesesTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var s1 = "{{{{{{";
+            var s2 = "(([]}))}";
+            var s3 = "(())[]{}";
+            var s4 = "(({[))]}";
+
+            var valid = new ValidParentheses();
+
+            Assert.False(valid.IsValid(s1));
+            Assert.False(valid.IsValid(s2));
+            Assert.True(valid.IsValid(s3));
+            Assert.False(valid.IsValid(s4));
+        }
+    }
+}
---
+++ Leecode/Solutions/20.ValidParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Leecode.Solutions
+{
+    public class ValidParentheses
+    {
+        private readonly Dictionary<char, char> dic;
+
+        public ValidParentheses()
+        {
+            this.dic = new Dictionary<char, char>()
+            {
+                {')', '(' },
+                {']', '[' },
+                {'}', '{' }
+            };
+        }
+
+        public bool IsValid(string s)
+        {
+            var charStr = s.ToCharArray();
+            var stack = new Stack<char>();
+
+            for (var i = 0; i < charStr.Length; i++)
+            {
+                var strKey = charStr[i];
+
+                if (this.dic.ContainsKey(strKey))
+                {
+                    var topElement = stack.Count == 0 ? '#' : stack.Pop();
+                    if (topElement != this.dic.GetValueOrDefault(strKey))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(strKey);
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
